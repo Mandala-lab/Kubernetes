9aa869a45eee75412d666ecaca7143512e1e75d7
Mandala <xiconz@qq.com>
2024-09-03 23:18:33 +0800

    fix: fix error

--- base-start.sh
+++ base-start.sh
@@ -15,7 +15,7 @@ chmod +x ./base/01-env/deb/03-allow-port.sh
 
 chmod +x ./base/02-cri/containerd/binarymode/01-install.sh
 #./base/02-cri/containerd/binarymode/01-install.sh --proxy=y --install=n --version="1.7.21"
-./base/02-cri/containerd/binarymode/01-install.sh --proxy=y --install=n --version="1.7.21"
+./base/02-cri/containerd/binarymode/01-install.sh --proxy --install --version="1.7.21"
 
 chmod +x ./base/02-cri/containerd/binarymode/02-config.sh
 ./base/02-cri/containerd/binarymode/02-config.sh
@@ -33,22 +33,15 @@ chmod +x ./base/02-cri/containerd/binarymode/03-install-runc.sh
 chmod +x ./base/03-components/03-socat/deb/install.sh
 ./base/03-components/03-socat/deb/install.sh
 
-chmod +x ./base/02-cri/containerd/binarymode/03-repo.sh
-./base/02-cri/containerd/binarymode/03-repo.sh --http_proxy="http://192.168.3.220:7890" --https_proxy="http://192.168.3.220:7890"
-
-# --remove 可选值: y, n
-# y: 删除旧安装
-# n: 保留旧安装, 但会重新下载安装覆盖
-# --version Kubernetes版本, 以vX.Y定义, 例如"v1.30"
-chmod +x ./base/03-components/deb/template.sh
-./base/03-components/deb/template.sh --proxy --update --remove --version="v1.30"
+#chmod +x ./base/02-cri/containerd/binarymode/03-repo.sh
+#./base/02-cri/containerd/binarymode/03-repo.sh --http_proxy="http://192.168.3.220:7890" --https_proxy="http://192.168.3.220:7890"
 
 # 如果需要手动上传, 那么请上传二进制文件到/tmp, 文件名为: crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz
 # CRICTL_VERSION: 版本, 例如v1.30.0
 # ARCH: 架构, 例如, amd64
 chmod +x ./base/03-components/01-crictl/01-install.sh
-#./base/03-components/01-crictl/01-install.sh --proxy=y --install=y --version="v1.31.1"
-./base/03-components/01-crictl/01-install.sh --version="v1.31.1"
+#./base/03-components/01-crictl/01-install.sh --proxy=y --install --version="v1.31.1"
+./base/03-components/01-crictl/01-install.sh --install --version="v1.31.1"
 
 chmod +x ./base/03-components/02-comm/binarymode/01-install-components.sh
 ./base/03-components/02-comm/binarymode/01-install-components.sh --remove=n --version="v1.31.1"
