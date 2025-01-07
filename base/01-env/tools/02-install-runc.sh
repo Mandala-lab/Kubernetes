45abd39ad2fe42b8b10096969a7bcb1f416f5f0f
Mandala <xiconz@qq.com>
2025-01-07 14:23:56 +0800

    feat: add check runc local file support

--- base/01-env/tools/02-install-runc.sh
+++ base/01-env/tools/02-install-runc.sh
@@ -5,7 +5,7 @@ set -e -o posix -o pipefail -x
 declare github_proxy=false
 declare github_proxy_url=""
 declare install=false
-declare version="v1.2.3"
+declare version="1.2.3"
 declare url=""
 
 while [[ $# -gt 0 ]]; do
@@ -52,10 +52,57 @@ set_arch() {
   echo "ARCH=${ARCH}"
 }
 
+check_local() {
+  echo "正在检查 /tmp 目录中是否存在 runc.${ARCH} 二进制文件"
+  if ls /tmp/runc.${ARCH} 1> /dev/null 2>&1; then
+    echo "找到 runc.${ARCH} 二进制文件，开始安装..."
+
+    # 安装 runc 到 /usr/local/sbin/
+    install -m 755 /tmp/runc.${ARCH} /usr/local/sbin/runc
+
+    # 删除下载的二进制文件
+    rm -rf /tmp/runc.${ARCH}
+
+    echo "runc 已成功更新到版本 $version"
+  else
+    echo "未找到 runc.${ARCH} 二进制文件，请确保文件已下载到 /tmp 目录。"
+  fi
+}
+
+check_runc_version() {
+  # 获取 runc 的版本信息
+  local_version=$(runc -v 2>&1 | grep -oP 'runc version \K[0-9]+\.[0-9]+\.[0-9]+')
+  version="1.2.4"
+
+  # 检查是否成功获取版本号
+  if [ -z "$local_version" ]; then
+    echo "无法获取 runc 的版本号。"
+    exit 1
+  fi
+
+  # 输出版本号
+  echo "本地 runc 版本: $local_version"
+  echo "期望版本: $version"
+
+  # 比较版本号
+  if [ "$local_version" = "$version" ]; then
+    echo "版本相等"
+    return  # 退出函数，不再执行后续代码
+  fi
+
+  # 使用 sort -V 进行版本号比较
+  if [ "$(printf "%s\n" "$local_version" "$version" | sort -V | head -n1)" = "$local_version" ]; then
+    echo "本地版本号小于期望版本号, 判断本地是否存在新版"
+    check_local
+  else
+    echo "本地版本号大于期望版本号"
+  fi
+}
+
 set_url () {
   if [[ -z $url ]];then
    echo "set default url"
-   url="https://github.com/opencontainers/runc/releases/download/${version}/runc.${ARCH}"
+   url="https://github.com/opencontainers/runc/releases/download/$v{version}/runc.${ARCH}"
   fi
 
   echo "github_proxy_url: $github_proxy_url"
@@ -97,6 +144,7 @@ verify () {
 
 main () {
   set_arch
+  check_runc_version
   set_url "$url"
   install_runc "$url" "$ARCH" "$version" "$install"
   verify
